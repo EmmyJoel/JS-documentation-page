c1c8849d3015292e1401f66d3ccd06376ac4b040
EmmyJoel <emmanueloje30@gmail.com>
2022-09-01 21:43:57 +0100

    updated

--- alert.js
+++ alert.js
@@ -1,1 +1,11 @@
-alert('This is Javascript')
+greeting()
+
+function greeting() {
+    let message1 = "This is a product of JavaScript.";
+    let message2 = "To experience the page responsiveness, RESIZE THE WINDOW";
+
+    let myMessage =  [message1 + " " + message2];
+    myMessage.push([' to the minimum'])
+    alert(myMessage)
+}
+
